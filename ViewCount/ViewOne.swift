dc7b6675332646138f04482dc0efd80c0fcea965
林暐潔 <Wei-Jie@linweijiede-MacBook-Pro.local>
2016-10-27 12:25:37 +0800

    Initial Commit

---
+++ ViewCount/ViewOne.swift
@@ -0,0 +1,43 @@
+//
+//  ViewOne.swift
+//  ViewCount
+//
+//  Created by 林暐潔 on 2016/10/27.
+//  Copyright © 2016年 林暐潔. All rights reserved.
+//
+
+import UIKit
+
+class ViewOne: UIViewController {
+    
+    @IBOutlet weak var showCountLabel: UILabel!
+    var count = 0
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        count += 1
+        showCountLabel.text = String(count)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ViewCount/ViewTwo.swift
@@ -0,0 +1,45 @@
+//
+//  ViewTwo.swift
+//  ViewCount
+//
+//  Created by 林暐潔 on 2016/10/27.
+//  Copyright © 2016年 林暐潔. All rights reserved.
+//
+
+import UIKit
+
+class ViewTwo: UIViewController {
+    
+    @IBOutlet weak var showCountLabelTwo: UILabel!
+    
+    var count = 0
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        count += 1
+        showCountLabelTwo.text = String(count)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
